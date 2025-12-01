576c82b84da40bef83542c852dedfcdbb875207f
Justin Auger <justinauger@gmail.com>
2025-12-01 15:21:14 +0000

    statically link.

--- benches/day01.rs
+++ benches/day01.rs
@@ -1,9 +1,10 @@
 use aoc_2025::day01;
 use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
+use std::fs;
 
-fn bench_day01(c: &mut Criterion) {
-    let input = std::fs::read_to_string("inputs/01.txt").unwrap();
+fn bench_day01_solve(c: &mut Criterion) {
+    let input = fs::read_to_string("inputs/01.txt").unwrap();
 
     c.bench_function("day01_solve", |b| {
         b.iter(|| {
@@ -12,5 +13,13 @@ fn bench_day01(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_day01);
+fn bench_day01_read_to_string(c: &mut Criterion) {
+    c.bench_function("day01_read_to_string", |b| {
+        b.iter(|| {
+            let _ = fs::read_to_string(black_box("inputs/01.txt")).unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, bench_day01_solve, bench_day01_read_to_string);
 criterion_main!(benches);
