09e8e15a3aeda90b7d09c310869d3e3eaf6e5455
Justin Auger <justinauger@gmail.com>
2025-12-03 17:09:36 +0000

    faster

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,15 +4,16 @@ fn main() {
     // let aoc_day = "1";
     // let aoc_input = "inputs/01.txt";
 
-    let input = std::fs::read_to_string(aoc_input).unwrap();
-
     if aoc_day == "1" {
+        let input = std::fs::read_to_string(aoc_input).unwrap();
         let (p1, p2) = aoc_2025::day01::solve(&input);
         println!(r#"{{"part_one": {}, "part_two": {}}}"#, p1, p2);
     } else if aoc_day == "2" {
+        let input = std::fs::read_to_string(aoc_input).unwrap();
         let (p1, p2) = aoc_2025::day02::solve(&input);
         println!(r#"{{"part_one": {}, "part_two": {}}}"#, p1, p2);
     } else if aoc_day == "3" {
+        let input = std::fs::read(aoc_input).unwrap();
         let (p1, p2) = aoc_2025::day03::solve(&input);
         println!(r#"{{"part_one": {}, "part_two": {}}}"#, p1, p2);
     } else {
--- src/day03.rs
+++ src/day03.rs
@@ -1,46 +1,62 @@
-pub fn solve(input: &str) -> (i64, i64) {
-    let lines = input.lines().collect::<Vec<&str>>();
+
+pub fn solve(bytes: &[u8]) -> (i64, i64) {
     let mut part1 = 0;
     let mut part2 = 0;
-    for line in lines {
-        // Part 1
-        // Find largest digit in the line (excluding the last digit)
-        let mut largest = 0;
-        let mut largest_index = 0;
-        for i in 0..line.len() - 1 {
-            let digit = line[i..i + 1].parse::<i64>().unwrap();
-            if digit > largest {
-                largest = digit;
-                largest_index = i;
-            }
+    
+    // Process line by line
+    let mut start = 0;
+    while start < bytes.len() {
+        // Find end of line (newline or end of input)
+        let mut end = start;
+        while end < bytes.len() && bytes[end] != b'\n' {
+            end += 1;
         }
-        let mut largest2 = 0;
-        for i in largest_index + 1..line.len() {
-            largest2 = std::cmp::max(largest2, line[i..i + 1].parse::<i64>().unwrap());
-        }
-        println!("largest: {}, largest2: {}", largest, largest2);
-        part1 += 10 * largest + largest2;
-
-        // Part 2
-        let mut min_index = 0;
-        let mut sum = 0;
-        for digit in 1..=12 {
-            // Find largest digit in line, exclude the last (12-digit) digits
-            let mut largest = 0;
+        
+        if start < end {
+            let line = &bytes[start..end];
+            
+            // Part 1 - Find largest digit in the line (excluding the last digit)
+            let mut largest_byte = b'0';
             let mut largest_index = 0;
-            for i in min_index..line.len() - (12 - digit) {
-                let digit = line[i..i + 1].parse::<i64>().unwrap();
-                if digit > largest {
-                    largest = digit;
+            for i in 0..line.len() - 1 {
+                if line[i] > largest_byte {
+                    largest_byte = line[i];
                     largest_index = i;
                 }
             }
-            min_index = largest_index + 1;
-            sum = sum * 10 + largest;
+            let mut largest2_byte = b'0';
+            for i in largest_index + 1..line.len() {
+                if line[i] > largest2_byte {
+                    largest2_byte = line[i];
+                }
+            }
+            let largest = (largest_byte - b'0') as i64;
+            let largest2 = (largest2_byte - b'0') as i64;
+            part1 += 10 * largest + largest2;
+
+            // Part 2
+            let mut min_index = 0;
+            let mut sum = 0;
+            for digit in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] {
+                // Find largest digit in line, exclude the last (12-digit) digits
+                let mut largest_byte = b'0';
+                let mut largest_index = 0;
+                for i in min_index..line.len() - (12 - digit) {
+                    if line[i] > largest_byte {
+                        largest_byte = line[i];
+                        largest_index = i;
+                    }
+                }
+                min_index = largest_index + 1;
+                let largest = (largest_byte - b'0') as i64;
+                sum = sum * 10 + largest;
+            }
+            part2 += sum;
         }
-        println!("sum: {}", sum);
-        part2 += sum;
+        
+        start = end + 1;  // Move past newline
     }
+    
     (part1, part2)
 }
 
@@ -50,18 +66,18 @@ mod tests {
 
     #[test]
     fn test() {
-        let input = std::fs::read_to_string("inputs/03.txt").unwrap();
+        let input = std::fs::read("inputs/03.txt").unwrap();
         assert_eq!(solve(&input), (17087, 169019504359949));
     }
 
     #[test]
     fn test_part1() {
-        let test_input = "987654321111111
+        let test_input = b"987654321111111
 811111111111119
 234234234234278
 818181911112111
 ";
 
-        assert_eq!(solve(&test_input), (357, 3121910778619));
+        assert_eq!(solve(test_input), (357, 3121910778619));
     }
 }
