fa9be5f5eeb6aa5d0c10e0a3bfeac3344ee04a48
Justin Auger <justinauger@gmail.com>
2025-12-08 18:03:52 +0000

    speed up sort.

--- quickbench.sh
+++ quickbench.sh
@@ -6,5 +6,5 @@ cargo build --release
 export AOC_DAY=$1
 export AOC_INPUT=$(./scripts/input_path.sh)
 
-hyperfine './target/x86_64-unknown-linux-musl/release/aoc-2025' --warmup 10 -N # --min-runs 50000
+hyperfine './target/x86_64-unknown-linux-musl/release/aoc-2025' --warmup 100 -N # --min-runs 50000
 # hyperfine './target/release/test' --warmup 100 -N
--- src/day08.rs
+++ src/day08.rs
@@ -39,15 +39,13 @@ pub fn solve(bytes: &[u8], num_connections: usize) -> (i64, i64) {
     let mut edges = Vec::new();
     for p_idx in 0..points.len() {
         for q_idx in p_idx + 1..points.len() {
-            edges.push((p_idx, q_idx));
+            let p = points[p_idx];
+            let q = points[q_idx];
+            let dist = ((p.0 - q.0).pow(2) + (p.1 - q.1).pow(2) + (p.2 - q.2).pow(2)) as u64;
+            edges.push((p_idx + 0x10000 * q_idx, dist));
         }
     }
-    edges.sort_unstable_by_key(|&e| {
-        let (p_idx, q_idx) = e;
-        let p = points[p_idx];
-        let q = points[q_idx];
-        (p.0 - q.0).pow(2) + (p.1 - q.1).pow(2) + (p.2 - q.2).pow(2)
-    });
+    edges.sort_unstable_by_key(|e| e.1);
 
     for (edge_idx, edge) in edges.iter().enumerate() {
         // Part 1: Find the 3 largest connected components
@@ -56,16 +54,13 @@ pub fn solve(bytes: &[u8], num_connections: usize) -> (i64, i64) {
             component_sizes.sort_unstable_by_key(|&size| std::cmp::Reverse(size));
             part1 = component_sizes[0] * component_sizes[1] * component_sizes[2];
         }
+        let p_idx = edge.0 & 0xFFFF;
+        let q_idx = edge.0 >> 16;
 
-        if label[edge.0] == label[edge.1] && edge_idx >= num_connections {
-            continue;
-        }
-
-        if label[edge.0] != label[edge.1] {
+        if label[p_idx] != label[q_idx] {
             // Merge the two labels
-            let (p_idx, q_idx) = edge;
-            let p_label = label[*p_idx];
-            let q_label = label[*q_idx];
+            let p_label = label[p_idx];
+            let q_label = label[q_idx];
             if label_ll[p_label].len() < label_ll[q_label].len() {
                 for q_idx in label_ll[q_label].iter() {
                     label[*q_idx] = p_label;
@@ -80,11 +75,11 @@ pub fn solve(bytes: &[u8], num_connections: usize) -> (i64, i64) {
                 label_ll[q_label].append(&mut p_list);
             }
             merges += 1;
-        }
 
-        if merges == points.len() - 1 {
-            let part2 = points[edge.0].0 * points[edge.1].0;
-            return (part1 as i64, part2);
+            if merges == points.len() - 1 {
+                let part2 = points[p_idx].0 * points[q_idx].0;
+                return (part1 as i64, part2);
+            }
         }
     }
 
